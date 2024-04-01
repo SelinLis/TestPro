fe9520967aae395420dda0e3183742452922bd1e
Admin <Admin@DESKTOP-3LFH1JO>
2024-04-01 12:16:17 +0800

    changed class Test.java

--- TestPro/src/Test.java
+++ TestPro/src/Test.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class Test {
-
+	public static void main(String args[]) {
+		Scanner gg = new Scanner(System.in);
+		String name = gg.next();
+		int age = gg.nextInt();
+		int bornYear = 2024 - age;
+		System.out.println("Your name:"+name);
+		System.out.println("Your born year:"+bornYear);
+		gg.close();
+	}
 }
